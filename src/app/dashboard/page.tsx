02bd9d14ff2d8ca1955c0b971ee196f17ed60c50
Daniel Stadler <danstad2012@gmail.com>
2024-10-16 16:27:25 -0700

    update nav; bring canvas to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
-export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dashboard</h1>
+'use client'
 
-    </div>
+import React, { useCallback, useEffect, useRef } from 'react'
+import { useAtom } from 'jotai'
+import { Box, Stack, Typography } from '@mui/material'
+import { getResourceList } from '@/utils/constants'
+import { resourceNodesData, transportNodesData } from '@/data'
+import { useCanvas, useHomeNode, useResourceNodes, useTransportNodes } from '@/hooks'
+import { resourcesAtom } from '@/atoms'
+
+const OverworldPage = () => {
+  const { ctx, canvasRef, drawFPS, clearWholeRect, handleClick } = useCanvas()
+
+  const [mainResources] = useAtom(resourcesAtom)
+  const { homeNode, drawHomeNode } = useHomeNode(ctx)
+  const { resourceNodes, drawResourceNodes } = useResourceNodes({
+    ctx,
+    homeNode,
+    resourceNodesData,
+  })
+  const { transportNodes, drawTransportNodes } = useTransportNodes({
+    ctx,
+    homeNode,
+    resourceNodes,
+    transportNodesData,
+  })
+
+  const rafIdRef = useRef<number | null>(null)
+  const gameLoop = useCallback((timestamp: number) => {
+    if (!canvasRef.current) return
+
+    clearWholeRect(canvasRef.current)
+    drawFPS(timestamp)
+
+    drawHomeNode()
+    drawResourceNodes()
+    drawTransportNodes()
+
+    rafIdRef.current = requestAnimationFrame(gameLoop)
+  }, [canvasRef, clearWholeRect, drawFPS, drawHomeNode, drawResourceNodes, drawTransportNodes])
+
+  useEffect(() => {
+    if (
+      !homeNode ||
+      !resourceNodes.length ||
+      !transportNodes.length ||
+      rafIdRef.current
+    ) return
+
+    rafIdRef.current = requestAnimationFrame(gameLoop)
+  }, [gameLoop, homeNode, resourceNodes, transportNodes])
+
+  return (
+    <Stack flexDirection="row" justifyContent="space-between">
+      <Box>
+        Home
+        <div>
+          <Typography>
+            {Object.keys(mainResources).length > 0 ? (
+              getResourceList({ resourceObject: mainResources })
+                .map(resource => <div key={resource}>{resource}</div>)
+            ) : 'No resources available'}
+          </Typography>
+        </div>
+      </Box>
+      <Box>
+        This is the Overworld Page Content
+        <canvas
+          ref={canvasRef}
+          width={800}
+          height={600}
+          className="border-2 border-purple-500 border-rounded"
+          onClick={handleClick}
+        />
+      </Box>
+    </Stack>
   )
 }
+
+export default OverworldPage
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,52 +1,50 @@
 import { ResourceRecord } from '@/types/node'
 import {
-  Map as MapIcon,
+  // Map as MapIcon,
 } from '@mui/icons-material'
 
 export const NAV_TABS = [
+  // {
+  //   // icon: <LocalOffer />,
+  //   title: 'Player',
+  //   route: 'player',
+  //   // matchPath: '/app/purchases/:id*',
+  // },
   {
+    // icon: ,
+    title: 'Resources',
+    route: 'resources',
+  },
+  {
+    // icon: ,
+    title: 'Crafting',
+    route: 'crafting',
+  },
+  {
+    // icon: ,
+    title: 'Research',
+    route: 'research',
+  },
+{
     // icon: <LocalOffer />,
     title: 'Civvies',
     route: 'civvies',
-    // matchPath: '/app/purchases/:id*',
-    // treatmentKeys: ['app-route-purchases'],
-  },
-  {
-    // icon: <LocalOffer />,
-    title: 'Player',
-    route: 'player',
-    // matchPath: '/app/purchases/:id*',
-    // treatmentKeys: ['app-route-purchases'],
-  },
-  // {
-  //   icon: ,
-  //   title: 'Village',
-  //   route: 'village',
-  // },
-  // {
-  //   // icon: ,
-  //   title: 'Research',
-  //   route: 'research',
-  // },
-  {
-    icon: MapIcon,
-    title: 'Overworld',
-    route: 'overworld',
   },
   // {
   //   // icon: ,
   //   title: 'Achievements',
   //   route: 'achievements',
-  // },{
-  //   // icon: ,
-  //   title: 'Resources',
-  //   route: 'resources',
   // },
-  // {
-  //   // icon: ,
-  //   title: 'Inventory',
-  //   route: 'inventory',
-  // },
+  {
+    // icon: ,
+    title: 'Store',
+    route: 'store',
+  },
+  {
+    // icon: ,
+    title: 'Village',
+    route: 'village',
+  },
 ]
 
 export const RESOURCES = {
