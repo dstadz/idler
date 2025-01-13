0ca963fdce707e6d6688e474c59d429cc068d9b1
Daniel Stadler <danstad2012@gmail.com>
2025-01-13 12:04:00 -0800

    init admin page

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,91 @@
+// components/DashboardLayout.tsx
+'use client'
+
+import React, { ReactNode, useEffect } from 'react'
+import { useAtom } from 'jotai'
+import { Stack } from '@mui/material'
+
+import NavStack from '@/components/NavStack'
+import SignOutButton from '@/components/SignOutbutton'
+import Canvas from '@/components/canvas/Canvas'
+import { moneyAtom, userAtom, userIdAtom } from '@/atoms'
+import { supabase } from '@/lib/supabase'
+import Button from '@/components/UI/Button'
+import { BUILDINGS, RESOURCES } from '@/utils/constants'
+
+const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  // const router = useRouter()
+  const [money] = useAtom(moneyAtom)
+  const [user, setUser] = useAtom(userAtom);
+  const [userId, setUserId] = useAtom(userIdAtom);
+
+  const buildingNodes = [
+    {
+      id: 1,
+      type: BUILDINGS.VILLAGE,
+      position: [4, 7],
+      status: 'active',
+      level: 1,
+      emoji: '🏠',
+      resources: {
+        [RESOURCES.WOOD.NAME]: 10,
+        [RESOURCES.FOOD.NAME]: 10,
+        [RESOURCES.STONE.NAME]: 10,
+      },
+    },
+  ]
+
+  useEffect(() => {
+    const { data, error } = supabase
+      .auth
+      .onAuthStateChange((event, session) => {
+      setUserId(session?.user?.id)
+    })
+
+    return () => {
+      supabase.auth.onAuthStateChange(null)
+    }
+  }, [setUserId])
+
+  useEffect(() => {
+    if (!userId) return
+    const fetchData = async () => {
+      const { data: users, error: dbError } = await supabase
+      .from('users')
+      .select('*')
+      .eq('id', userId)
+
+      if (dbError) {
+        console.error("Database error:", dbError)
+      } else {
+        setUser(users[0])
+      }
+    }
+    fetchData()
+  }, [userId, supabase])
+
+  if (!userId) {
+    return <div>User not logged in</div>
+}
+
+  return (
+    <Stack style={{ width: '100%', height: '100%' }} flexDirection='row'>
+      <NavStack />
+      <Stack flexDirection="column" sx={{ border: '5px solid green' }}>
+        <Header money={money} user={user} />
+        {children}
+        <Canvas />
+      </Stack>
+    </Stack>
+  )
+}
+
+export default DashboardLayout
+
+const Header = ({ money, user }: { money: number; user: any}) => (
+  <Stack flexDirection="row" sx={{ border: '3px solid blue' }}>
+    {user && <h1>{user.name}s Dashboard</h1>}
+  ${money}
+    <SignOutButton />
+  </Stack>
+)
---
+++ src/app/admin/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+import { Stack } from '@mui/material'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const data = await supabase.from('users').select('*')
+
+  return (
+    <Stack sx={{ border: '3px solid purple' }}>
+      <h1>Admin Page</h1>
+    </Stack>
+  )
+}
